264f4ea286d0ac50dae4f79e1789668e89fd8387
Vitor Lippi <vitorlippilima.vl@gmail.com>
2025-07-28 16:37:16 -0300

    (Profile): Rota Dinamica adicionada

--- wapp/src/layout/MainLayout.jsx
+++ wapp/src/layout/MainLayout.jsx
@@ -2,6 +2,7 @@ import { House } from 'lucide-react';
 import { User } from 'lucide-react';
 import { Settings } from 'lucide-react';
 import { useAuth } from '../context/AuthContext';
+import { Link } from 'react-router-dom';
 
 const MainLayout = ({ children }) => {
 
@@ -17,7 +18,7 @@ const MainLayout = ({ children }) => {
                 <nav className="flex flex-col gap-4 sticky">
                     <h1 className="text-2xl font-bold mb-6">Wapp</h1>
                     <a href="/" className="flex items-center gap-2"> <House /> Inicío</a>
-                    <a href="/profile/:username" className="flex items-center gap-2"> <User/> perfil</a>
+                    <Link to={`/profile/${user?.username}`} className="flex items-center gap-2"> <User/> perfil</Link>
                     <a href="/configuracao" className="flex items-center gap-2"> <Settings /> Configurações</a>
                     <button onClick={logout} className="mt-2 text-red-500 underline">Sair</button>
                 </nav>
